cdd4e2d721fc94d3ed52f84fc324cb45e5edbb29
Sebastian Willing <sewi@cpan.org>
2010-03-31 10:21:26 +0000

    Import of SEWI/Perl-Staff-0.03 from CPAN.
    gitpan-cpan-distribution: Perl-Staff
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SEWI/Perl-Staff-0.03.tar.gz
    gitpan-cpan-author:       SEWI
    gitpan-cpan-maturity:     released

--- lib/Perl/Staff.pm
+++ lib/Perl/Staff.pm
@@ -11,11 +11,11 @@ Perl::Staff - People working for Perl
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
@@ -53,14 +53,31 @@ are usually shown on the blog pages).
 
 Event planning page: L<http://www.perlfoundation.org/perl5/index.cgi?events_2010_fosdem>
 
-=head3 Gabor Szabo
+=head3 Gábor Szabó
 
 (main coordinator)
-Event report: L<http://szabgab.com/blog/2010/02/1266052953.html>
+
+Event report: 
+
+=over 4
+
+=item * FOSDEM report
+
+L<http://szabgab.com/blog/2010/02/1266052953.html>
+
+=back
 
 =head3 Claudio Ramirez
 
-Event report: L<http://nxadm.wordpress.com/2010/02/19/fosdem-2010-impressions/>
+Event report: 
+
+=over 4
+
+=item * Fosdem 2010 impressions
+
+L<http://nxadm.wordpress.com/2010/02/19/fosdem-2010-impressions/>
+
+=back
 
 =head3 Dirk DeNijs
 
@@ -68,33 +85,107 @@ Event report: L<http://nxadm.wordpress.com/2010/02/19/fosdem-2010-impressions/>
 
 =head3 Erik Colson
 
+=head3 Salve J. Nilsen
+
+L<http://www.nuug-foundation.no/no/FOSDEM-2010/> in Norwegian
+
 =head2 CeBit
 
 Event planning page: L<http://www.perlfoundation.org/perl5/index.cgi?events_2010_cebit>
 
-=head3 Gabor Szabo
+=head3 Gábor Szabó
 
 (main coordinator)
 
-Event report: L<http://szabgab.com/blog/2010/03/1267974609.html>
+Event reports:
+
+=over 4
+
+=item * Two days into CeBIT
+
+L<http://szabgab.com/blog/2010/03/1267676778.html>
+
+=item * Perl on CeBIT
+
+L<http://szabgab.com/blog/2010/03/1267974609.html>
+
+=item * Another report about CeBIT, Perl and the community
+
+L<http://szabgab.com/blog/2010/03/1268387636.html>
+
+=back
 
 =head3 Renee Bäcker
 
 (main coordinator)
 
-Did a talk on Perl on CeBit.
+Did a talk on Perl on CeBit:
+
+=over 4
+
+=item * Slides
+
+L<http://renee-baecker.de/talks/CeBIT2010.pdf>
+
+=item * Skript 
+
+L<http://renee-baecker.de/talks/CeBIT2010_Skript.pdf>
+
+=back
 
 Event reports:
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-0.html>
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-1.html>
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-2.html>
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-3.html>
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-4.html>
-	L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-5.html>
+
+=over 4
+
+=item * Perl@CeBIT - Tag 0
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-0.html>
+
+=item * Perl@CeBIT - Tag 1
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-1.html>
+
+=item * Perl@CeBIT - Tag 2
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-2.html>
+
+=item * Perl@CeBIT - Tag 3
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-3.html>
+
+=item * Perl@CeBIT - Tag 4
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-4.html>
+
+=item * Perl@CeBIT - Tag 5
+
+L<http://reneeb-perlblog.blogspot.com/2010/03/perlcebit-tag-5.html>
+
+=back
+
+=head3 Robert 'phaylon' Sedlacek
+
+Event report: 
+
+=over 4
+
+=item * Perl in Germany - My CeBIT Recap
+
+L<http://blogs.perl.org/user/robert_phaylon_sedlacek/2010/03/perl-in-germany-my-cebit-recap.html>
+
+=back
 
 =head3 Sebastian Willing
 
-Event report: L<http://padre-ide.de/blog/?p=111>
+Event report:
+
+=over 4
+
+=item * CeBit 2010 is over
+
+L<http://padre-ide.de/blog/?p=111>
+
+=back
 
 =head3 Torsten Raudssus
 
@@ -102,11 +193,27 @@ use Moose;
 
 =head3 Thomas Fahle
 
+Event report: 
+
+=over 4
+
+=item * Perl@CeBIT2010
+
+L<http://thomas-fahle.blogspot.com/2010/03/perlcebit2010.html>
+
+=back
+
 =head3 Bernhard Schmalhofer
 
 =head3 Oliver Krüger
 
 =head3 Michael Daum
+
+=item * Foswiki on CeBit 2010
+
+L<http://blog.foswiki.org/2010/03/foswiki-on-cebit-2010/>
+
+=back
 
 =cut
 
